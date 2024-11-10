ec91080ee2f8888e656c5b5de2f18a81e755f03d
Phybar <tim.e.janke@gmail.com>
2024-11-10 20:26:15 +1000

    Turn added

--- main.py
+++ main.py
@@ -3,22 +3,32 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	print ("Starting asteroids!")
 	print (f"Screen width: {SCREEN_WIDTH}")
 	print (f"Screen height: {SCREEN_HEIGHT}")
 
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
 	
 	while True:
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+		
+		player.update(dt)
 		screen.fill((0, 0, 0))
+		player.draw(screen)
 		pygame.display.flip()
+		clock.tick(60)
+		dt = (clock.tick(60) / 1000)
 
 if __name__ == "__main__":
 	main()
