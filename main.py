3c062579821795f5ce51fdf70e490d2f4fcac710
Phybar <tim.e.janke@gmail.com>
2024-11-11 13:26:59 +1000

    Groups in the main

--- main.py
+++ main.py
@@ -5,10 +5,21 @@ import pygame
 from constants import *
 from player import *
 
+
+
+
+
+
 def main():
 	pygame.init()
 	clock = pygame.time.Clock()
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+
 	dt = 0
+	
 	print ("Starting asteroids!")
 	print (f"Screen width: {SCREEN_WIDTH}")
 	print (f"Screen height: {SCREEN_HEIGHT}")
@@ -16,19 +27,30 @@ def main():
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 	player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+
+
+	
 	
 	while True:
 
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
+			
 		
-		player.update(dt)
+		for obj in updatable:
+			obj.update(dt)
+
 		screen.fill((0, 0, 0))
-		player.draw(screen)
+
+		for obj in drawable:
+			obj.draw(screen)
+		
+
 		pygame.display.flip()
-		clock.tick(60)
-		dt = (clock.tick(60) / 1000)
+
+		# Limit FPS to 60
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
--- player.py
+++ player.py
@@ -3,10 +3,13 @@ from circleshape import *
 from constants import *
 
 
+
 class Player(CircleShape):
+   
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        
 
 
     # in the player class
@@ -31,3 +34,12 @@ class Player(CircleShape):
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
